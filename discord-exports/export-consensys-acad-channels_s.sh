9eba3b2e829eae401731bd05190d53c1615265b1
Gene Stakhov <70670631+gstakhov@users.noreply.github.com>
2021-10-18 13:01:18 -0400

    Add files via upload

---
+++ discord-exports/export-consensys-acad-channels_s.sh
@@ -0,0 +1,14 @@
+# Please read usage instructions for DiscordChatExporter (https://github.com/Tyrrrz/DiscordChatExporter/wiki/macOS-usage-instructions) CLI
+# Note the token and channel ID (https://github.com/Tyrrrz/DiscordChatExporter/wiki/Obtaining-Token-and-Channel-IDs)
+
+cd [path_to_DiscordChatExporter.Cli.dll]
+dotnet DiscordChatExporter.Cli.dll export -t YOUR_USER_TOKEN -c 880098414350729287 # 0-orientation
+dotnet DiscordChatExporter.Cli.dll export -t YOUR_USER_TOKEN -c 880098526720315402 # 1-fundamentals
+dotnet DiscordChatExporter.Cli.dll export -t YOUR_USER_TOKEN -c 880098633511469126 # 2-ethereum-basics
+dotnet DiscordChatExporter.Cli.dll export -t YOUR_USER_TOKEN -c 880098689673228309 # 3-smart-contracts
+dotnet DiscordChatExporter.Cli.dll export -t YOUR_USER_TOKEN -c 880098752126398504 # 4-developer-tooling
+dotnet DiscordChatExporter.Cli.dll export -t YOUR_USER_TOKEN -c 880098814122405918 # 5-defi-n-daos
+dotnet DiscordChatExporter.Cli.dll export -t YOUR_USER_TOKEN -c 880098885069054023 # 6-additional-topics
+dotnet DiscordChatExporter.Cli.dll export -t YOUR_USER_TOKEN -c 880098961745149973 # 7-layer-2s-and-scalability
+dotnet DiscordChatExporter.Cli.dll export -t YOUR_USER_TOKEN -c 880099031970369656 # 8-eth-2-the-merge
+dotnet DiscordChatExporter.Cli.dll export -t YOUR_USER_TOKEN -c 880099172269830194 # 9-next-steps
